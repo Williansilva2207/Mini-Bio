ea3923dba6add67512c7bce213f5e6e8087867da
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2025-08-21 21:52:37 +0000

    Minha mini bio

---
+++ mini_bio/app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <> 
+    <h1 id="Name"> Willian Rodrigues </h1>
+    <img  src="/willian.jpg" alt="Willzinho" width="271" height="270" className="img-profissional"/>
+    <p class="information">Estudante de Ciência da Computação</p>
+    <p class="information">Torcedor do Sport</p>
+    <p class="information">Cristão e leitor</p>
+   </>
+    
+  );
+}
